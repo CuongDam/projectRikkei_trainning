d09a011d6f5c77d430c89dfda731b0ac8e63f6d7
cuong_dam <cuongdm@rikkeisoft.com>
2022-03-28 16:51:57 +0700

    react-router 28/3/22

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-
+import { useState } from 'react';
 import './App.css';
+import React  from 'react';
 
+function handleSubmit(name, age){
+  //CALL API
+  
+}
 function App() {
+  
+  const [name, setName] = useState();
+  const [age, setAge] = useState();
+  
   return (
     <div className="App">
-    
+    <form>
+      <label>Enter your name
+        <input 
+         value ={name}
+          type="text\" 
+          onChange={e => setName(e.target.value)}
+          ></input>
+      </label>
+
+        <label>Enter your age
+        <input   
+        value={age}
+        type='text' 
+        onChange={e => setAge(e.target.value)}
+        ></input>
+        </label>
+
+        <button onClick={() => handleSubmit}>submit</button>
+
+    </form>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Blog from './pages/Blogs';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Layout from './pages/Layout';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+export default function Appp(){
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}></Route>
+        <Route index element={<Home />}></Route>
+        <Route path='blogs' element={<Blog />}></Route>
+        <Route path='contact' element={<Contact />}></Route>
+      </Routes>
+    </BrowserRouter>
 );
+}
+ReactDOM.render(<Appp />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
